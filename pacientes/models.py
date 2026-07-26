d1faa8b1c9fb87ee77df60c0a56c4014b67881ee
Ailen Martin <40954611@terciariourquiza.edu.ar>
2026-07-26 19:46:36 -0300

    Cree la tabla pte

--- pacientes/models.py
+++ pacientes/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Paciente(models.Model):
+    dni = models.CharField(max_length=20, primary_key=True)
+    nombrePaciente = models.CharField(max_length=20)
+    apellidoPaciente = models.CharField(max_length=20)
+    fechaNacimiento = models.DateField()
+    domicilio = models.CharField(max_length=50, blank=True)
+    localidad = models.CharField(max_length=50, blank=True)
+    correoElectronico = models.EmailField(max_length=50)
+    telefono = models.CharField(max_length=20)
+    obraSocial = models.CharField(max_length=20, blank=True)
+    sexo = models.CharField(max_length=1, blank=True)
+
+    def __str__(self):
+        return f"{self.apellidoPaciente}, {self.nombrePaciente}"

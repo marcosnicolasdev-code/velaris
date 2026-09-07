d731e71673702e64820b889764887bcf9f4dc27d
So7i <47508760@terciariourquiza.edu.ar>
2026-09-06 21:31:44 -0300

    Update admin.py

--- tratamientos/admin.py
+++ tratamientos/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from .models import Tratamiento, PlanPago ,Sesion, Receta
-admin.site.register(Tratamiento)
 # Register your models here.
 admin.site.register(Tratamiento)
 admin.site.register(PlanPago)

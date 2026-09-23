64ba4f8d2e8fd11b2a14c394bcecce826666340c
Marcos Nicolas Gomez Ramunno <marcosnicolas.dev@gmail.com>
2026-09-22 22:09:57 -0300

    arreglo de merge duplicado

--- tratamientos/apps.py
+++ tratamientos/apps.py
@@ -2,8 +2,4 @@ from django.apps import AppConfig
 
 
 class TratamientosConfig(AppConfig):
-<<<<<<< HEAD
     name = 'tratamientos'
-=======
-    name = 'tratamientos'
->>>>>>> feature/models-tratamientos
--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,12 +1,7 @@
 from django.contrib import admin
-<<<<<<< HEAD
-from .models import Profesional, Caja, MovimientoCaja
+from .models import Profesional, Caja, MovimientoCaja, Usuario
 
 admin.site.register(Profesional)
 admin.site.register(Caja)
 admin.site.register(MovimientoCaja)
-=======
-from .models import Usuario
-# Register your models here.
 admin.site.register(Usuario)
->>>>>>> feature/models-tratamientos
